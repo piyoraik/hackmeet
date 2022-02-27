43f59248efe20b25f901a289da9a5cc530b43346
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-02-28 02:20:52 +0900

    channel追加のmutationを追加

--- src/channel/channel.resolver.ts
+++ src/channel/channel.resolver.ts
@@ -1,6 +1,10 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GraphqlAuthGuard } from 'src/authz/authz.guard';
+import { JwtPayload } from 'src/authz/types/jwt-payload.type';
 import { Channel } from 'src/entity/channel.entity';
 import { ChannelService } from './channel.service';
+import { InputChannelDTO } from './dto/create.channel.dto';
 
 @Resolver()
 export class ChannelResolver {
@@ -9,5 +13,15 @@ export class ChannelResolver {
   @Query(() => Channel, { name: 'findOneChannel' })
   findOneId(@Args('id') id: string) {
     return this.channelService.findOne({ id });
+  }
+
+  @UseGuards(GraphqlAuthGuard)
+  @Mutation(() => Channel, { name: 'createChannel' })
+  create(
+    @Args('createChannel') channel: InputChannelDTO,
+    @Context() context: any,
+  ) {
+    const payload = context.req.user as JwtPayload;
+    return this.channelService.create(channel, payload.sub);
   }
 }
